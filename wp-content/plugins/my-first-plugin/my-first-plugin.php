0efa4763b5dc0158d90642be02a9e70bb24a1430
Syed Ahsan Raza <75121164+syedahsanraza1@users.noreply.github.com>
2023-09-11 22:05:32 +0500

    updating wordpress and creating menu items
    updating wordpress and creating menu items

--- wp-content/plugins/my-first-plugin/my-first-plugin.php
+++ wp-content/plugins/my-first-plugin/my-first-plugin.php
@@ -1,4 +1,4 @@
-</php
+<?php
 
 /*
  * Plugin Name:       My first Plugin
@@ -10,8 +10,75 @@
  * Text Domain:       my-first-plugin
  */
 
-
- if ( is_admin() ) {
-    // we are in admin mode
-    require_once __DIR__ . '/admin/plugin-name-admin.php';
+if (! defined('WPINC')){
+    die;
 }
+
+
+
+
+function ahsan_dashboard_menu() {
+    add_menu_page(
+        'Ahsan Dashboard',
+        'Ahsan',
+        'manage_options',
+        'ahsan-dashboard',
+        'ahsan_dashboard_page',
+        'dashicons-dashboard',
+        10
+        
+    );
+
+
+    // Add sub-menu pages
+    add_submenu_page(
+        'ahsan-dashboard',
+        'Feature 1',
+        'Feature 1',
+        'manage_options',
+        'ahsan-feature1',
+        'ahsan_feature1_page',
+        100
+    );
+
+    add_submenu_page(
+        'ahsan-dashboard',
+        'Feature 2',
+        'Feature 2',
+        'manage_options',
+        'ahsan-feature2',
+        'ahsan_feature2_page'
+    );
+}
+
+// Function to display content on the dashboard submenu pages
+function ahsan_dashboard_page() {
+    ?>
+    <div class="wrap">
+        <h2>Ahsan Dashboard</h2>
+        <p>Welcome to the Ahsan Dashboard! This is a custom dashboard menu created for Ahsan.</p>
+        <p>You can add your content and functionality here.</p>
+    </div>
+    <?php
+}
+
+function ahsan_feature1_page() {
+    ?>
+    <div class="wrap">
+        <h2>Feature 1</h2>
+        <p>This is Feature 1 content. You can add your custom content and functionality here.</p>
+    </div>
+    <?php
+}
+
+function ahsan_feature2_page() {
+    ?>
+    <div class="wrap">
+        <h2>Feature 2</h2>
+        <p>This is Feature 2 content. You can add your custom content and functionality here.</p>
+    </div>
+    <?php
+}
+
+// Hook to add the dashboard menu and sub-menu pages
+add_action('admin_menu', 'ahsan_dashboard_menu');
